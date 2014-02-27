68addb15a0bf8e6144c90fee95c189cb284f282e
Paul Famiglietti <p0seid0n_@hotmail.com>
2014-02-26 21:32:50 -0800

    Added title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,9 +7,14 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('DailyDocket')
     end
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("DailyDocket | Home")
+      expect(page).to have_title("DailyDocket")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
   
