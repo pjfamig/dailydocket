95ee3c5a57c185aabce9622f4ee5c30648c39125
Paul Famiglietti <p0seid0n_@hotmail.com>
2014-02-26 20:37:37 -0800

    Added StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Dailydocket::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/about"
+  get "static_pages/rules"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
