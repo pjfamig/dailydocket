533a0dd7a90cde05a5892cfedd34a007b79616ea
Paul Famiglietti <p0seid0n_@hotmail.com>
2014-02-28 16:32:38 -0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Dailydocket::Application.routes.draw do
   resources :users  
+  resources :sessions, only: [:new, :create, :destroy]
   root  'static_pages#home'
-  match '/about', to: 'static_pages#about', via: 'get'
-  match '/rules', to: 'static_pages#rules', via: 'get'
-  match '/signup',  to: 'users#new',        via: 'get'
+  match '/about',   to: 'static_pages#about', via: 'get'
+  match '/rules',   to: 'static_pages#rules', via: 'get'
+  match '/signup',  to: 'users#new',          via: 'get'
+  match '/signin',  to: 'sessions#new',       via: 'get'
+  match '/signout', to: 'sessions#destroy',   via: 'delete'
   
   
   # The priority is based upon order of creation: first created -> highest priority.
