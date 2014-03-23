2aa66f65f52e3978a540f760b0335242bd74a7a7
Paul Famiglietti <p0seid0n_@hotmail.com>
2014-03-22 21:36:23 -0700

    Updated production env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,8 @@ Dailydocket::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
+
 
   # Generate digests for assets URLs.
   config.assets.digest = true
