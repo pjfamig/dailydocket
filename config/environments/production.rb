319be486dbf13ad682be9fd2359e4d3ae79ad9d8
Paul Famiglietti <p0seid0n_@hotmail.com>
2014-04-11 19:54:19 -0700

    Glyphicons for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Dailydocket::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
 
 
